296fbfbfeabec86abc7d25bd29ecfa2f28cbdc47
jonlee48 <jonathan.lee48@gmail.com>
2020-08-14 10:33:57 -0400

    pi py scripts

---
+++ pi/led.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# adapted from rpi_ws281x library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+
+# LED Wires
+# White - GND
+# Red - 5V
+# Green - I/O, Pin 12, AKA GPIO 18
+
+
+import time
+from rpi_ws281x import *
+import argparse
+
+
+# LED strip configuration:
+LED_COUNT      = 60      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+# Define functions which animate LEDs in various ways.
+def fill(strip, color, wait_ms=0):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+
+# TODO:
+#def glow(color):
+#def fade(color):
+
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print ('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+
+        while True:
+            
+
+            fill(strip, Color(255, 255, 255))  # white 
+            time.sleep(2)
+            fill(strip, Color(255, 0, 0))  # red
+            time.sleep(2)
+            fill(strip, Color(0, 255, 0))  # green
+            time.sleep(2)
+
+
+
+
+            print ('Color cycle: white, green, red.')
+    except KeyboardInterrupt:
+        if args.clear:
+            fill(strip, Color(0,0,0), 10)
